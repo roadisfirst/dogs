4deb2065f7e45c7495956a2bbf5ff897dba3a788
roadisfirst <roadisfirst@gmail.com>
2021-04-23 00:03:46 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+import Layout from './components/Layout/Layout';
+// import Classes from 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Layout>
+        <p>Test</p>
+      </Layout>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +22,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
