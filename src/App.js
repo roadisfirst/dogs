2a6d2f798b0bf18b25cb58725016ffe6dd1494e3
roadisfirst <roadisfirst@gmail.com>
2021-06-22 22:57:01 +0300

    SingleBreed component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
+import Content from './hoc/Content/Content';
 import Home from './components/Home/Home';
 
 import classes from './App.css';
 import MainMenu from './containers/MainMenu/MainMenu';
 import Breeds from './containers/Breeds/Breeds';
+import SingleBreed from './containers/Breeds/SingleBreed/SingleBreed';
 
 const App = () => {
   return (
@@ -15,9 +17,11 @@ const App = () => {
           <MainMenu />
           <Switch>
             <Route path="/pet-breeds" exact component={Breeds} />
-            <Route path="/" component={Home} />
+            <Route path="/pet-breeds/:id" exact component={SingleBreed} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/" />
           </Switch>
-          </Layout>
+        </Layout>
       </div>
   );
 }
