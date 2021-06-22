ccbe3e1303adc744e6692b1b004590307f87b2a6
roadisfirst <roadisfirst@gmail.com>
2021-06-22 23:04:49 +0300

    Form

--- src/components/UI/Form/Form.js
+++ src/components/UI/Form/Form.js
@@ -1,13 +1,19 @@
 import React, { Fragment } from 'react';
 import classes from './Form.css';
 
+//import searchItem from '../../../assets/images/icons/search-item.png';
+import * as Assets from '../../../assets/images/index';
+
 const form = () => (
     <Fragment>
         <form className={classes.Form}>
             <input 
                 className={classes.Input}
                 placeholder={'Search for breeds by name'} />
-            <button className={classes.Button}><img src="../../../assets/images/icons/search.svg" /></button>
+            {/* <button className={classes.Button}><img src={searchItem} /></button> */}
+            <button className={classes.Button}>
+                <img src={Assets.icons.searchItem.default} className={classes.SearchItemImg} />
+            </button>
         </form>
     </Fragment>
 )
