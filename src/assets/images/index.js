d3986b49ea4bcf1d656a24ee54ebe223164090b4
roadisfirst <roadisfirst@gmail.com>
2021-06-22 22:59:59 +0300

    assets folder

---
+++ src/assets/images/index.js
@@ -0,0 +1,14 @@
+export const icons = {
+    get liked() {
+        return require('./icons/liked.svg');
+    },
+    get disliked() {
+        return require('./icons/disliked.svg');
+    },
+    get favourite() {
+        return require('./icons/favourite.svg');
+    },
+    get searchItem() {
+        return require('./icons/search.svg');
+    }
+}
